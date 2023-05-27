c2ebe358377374c95b13e9e3bfb3c86c8096f07d
root <root@ip-172-31-27-3.ec2.internal>
2023-05-27 08:04:17 +0000

    update index.html

--- update_version.sh
+++ update_version.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cat index_var.html | sed  "s/temp.*/${DOCKER_TAG}/g" > index.html
+cat index_var.html | sed  "s/at.*/${DOCKER_TAG}/g" > index.html
 cat deploy-pod.yml | sed "s/repo.*/${DOCKER_TAG}/g" > deploy.yml
