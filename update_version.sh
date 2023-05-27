762da4f55d735112146ba53b37cbc6a7b4a71a44
root <root@ip-172-31-27-3.ec2.internal>
2023-05-27 07:56:16 +0000

    updated deployment variable

--- update_version.sh
+++ update_version.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cat index_var.html | sed  "s/temp.*/${DOCKER_TAG}/g" > index.html
-cat deploy-pod.yml | sed "s/temp.*/${DOCKER_TAG}/g" > deploy.yml
+cat deploy-pod.yml | sed "s/repo.*/${DOCKER_TAG}/g" > deploy.yml
